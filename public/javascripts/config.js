590f32c193884a8ac05584d1196ef951e4da30b9
Roy Kolak <roy.kolak@gmail.com>
2010-09-23 08:47:28 -0500

    added more builds to config and adjusted build project padding

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -1,4 +1,17 @@
 var config = {
-  people:['@roybot5000', '@tomkersten', '@paulsexton'],
-  builds:['Arlis', 'Codefumes', 'Codefumes Android']
+  people:[
+    '@roybot5000', 
+    '@tomkersten', 
+    '@paulsexton'
+  ],
+  builds:[
+    'ArlisWebsite', 
+    'Codefumes Android', 
+    'Dashboard', 
+    'LibTdt ReleaseBot', 
+    'SB0 Test', 
+    'Sound Booth 0', 
+    'Sound Booth 1',
+    'Sound Booth 2'
+  ]
 }
