d9401ac70a389f00a875c6feb5076e3f1ae8d218
Roy Kolak <roy.kolak@gmail.com>
2010-10-08 11:20:21 -0500

    updated urls to new hudson server. removed an old project

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -7,35 +7,31 @@ var config = {
   builds:[
     {
       name:'ArlisWebsite', 
-      url:'http://10.1.71.122:8080/job/ArlisWebsite/lastBuild/api/json',
+      url:'http://builder.gn-research/job/ArlisWebsite/lastBuild/api/json',
       sound:true
-    }, {
-      name:'Codefumes Android',
-      url:'http://10.1.71.122:8080/job/CodeFumes_Android/lastBuild/api/json',
-      sound:true
-    }, {
+    },{
       name:'Dashboard',
-      url:'http://10.1.71.122:8080/job/Dashboard/lastBuild/api/json',
+      url:'http://builder.gn-research/job/Dashboard/lastBuild/api/json',
       sound:true
     }, {
       name:'LibTdt ReleaseBot',
-      url:'http://10.1.71.122:8080/job/LibTdt%20ReleaseBot/lastBuild/api/json',
+      url:'http://builder.gn-research/job/LibTdt%20ReleaseBot/lastBuild/api/json',
       sound:false
     }, {
       name:'SB0 Test',
-      url:'http://10.1.71.122:8080/job/SB0%20Test/lastBuild/api/json',
+      url:'http://builder.gn-research/job/SB0%20Test/lastBuild/api/json',
       sound:false
     }, {
       name:'Sound Booth 0', 
-      url:'http://10.1.71.122:8080/job/Sound%20Booth%200/lastBuild/api/json',
+      url:'http://builder.gn-research/job/Sound%20Booth%200/lastBuild/api/json',
       sound:false
     }, {
       name:'Sound Booth 1',
-      url:'http://10.1.71.122:8080/job/Sound%20Booth%201/lastBuild/api/json',
+      url:'http://builder.gn-research/job/Sound%20Booth%201/lastBuild/api/json',
       sound:false
     }, {
       name:'Sound Booth 2',
-      url:'http://10.1.71.122:8080/job/Sound%20Booth%202/lastBuild/api/json',
+      url:'http://builder.gn-research/job/Sound%20Booth%202/lastBuild/api/json',
       sound:false
     }    
   ]
