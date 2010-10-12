395130863b25df6996183c9589033070668dd9ed
Roy Kolak <roy.kolak@gmail.com>
2010-10-12 15:21:46 -0500

    added sound for libtdt

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -20,7 +20,7 @@ var config = {
     }, {
       name:'LibTdt',
       url:'http://phobos:8080/job/LibTdt/lastBuild/api/json',
-      sound:false
+      sound:true
     }// , {
     //       name:'SB0 Test',
     //       url:'http://builder.gn-research/job/SB0%20Test/lastBuild/api/json',
