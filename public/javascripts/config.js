0f50ad9fdc16d2a20acbb950a294e75d1eede46e
Roy Kolak <roy.kolak@gmail.com>
2011-01-03 14:48:08 -0600

    updated old hostnames

--- public/javascripts/config.js
+++ public/javascripts/config.js
@@ -2,10 +2,10 @@ var config = {
   projects:[
     {
       name:'ArlisWebsite',
-      url:'http://builder.gn-research/job/ArlisWebsite/lastBuild/api/json'
+      url:'http://builder.research/job/ArlisWebsite/lastBuild/api/json'
     },{
       name:'Dashboard',
-      url:'http://builder.gn-research/job/Dashboard/lastBuild/api/json'
+      url:'http://builder.research/job/Dashboard/lastBuild/api/json'
     }, {
       name:'S2 Circuit',
       url:'http://phobos:8080/job/S2%20circuit/lastBuild/api/json'
@@ -17,28 +17,28 @@ var config = {
       url:'http://phobos:8080/job/CRTools/lastBuild/api/json'
     }, {
       name:'Eup',
-      url:'http://builder.gn-research/job/Eup/lastBuild/api/json'
+      url:'http://builder.research/job/Eup/lastBuild/api/json'
     }, {
       name:'Dispenser',
-      url:'http://builder.gn-research/job/Dispenser/lastBuild/api/json'
+      url:'http://builder.research/job/Dispenser/lastBuild/api/json'
     }
   ],
   pings:[
     {
       name:'Phobos',
-      url:'http://builder.gn-research/job/Ping%20Phobos/lastBuild/api/json'
+      url:'http://builder.research/job/Ping%20Phobos/lastBuild/api/json'
     }, {
       name:'Hermes',
-      url:'http://builder.gn-research/job/Ping%20Hermes/lastBuild/api/json'
+      url:'http://builder.research/job/Ping%20Hermes/lastBuild/api/json'
     }, {
       name:'SB 0',
-      url:'http://builder.gn-research/job/Ping%20Sound%20Booth%200/lastBuild/api/json'
+      url:'http://builder.research/job/Ping%20Sound%20Booth%200/lastBuild/api/json'
     }, {
       name:'SB 1',
-      url:'http://builder.gn-research/job/Ping%20Sound%20Booth%201/lastBuild/api/json'
+      url:'http://builder.research/job/Ping%20Sound%20Booth%201/lastBuild/api/json'
     }, {
       name:'SB 2',
-      url:'http://builder.gn-research/job/Ping%20Sound%20Booth%202/lastBuild/api/json'
+      url:'http://builder.research/job/Ping%20Sound%20Booth%202/lastBuild/api/json'
     }
   ]
 }
